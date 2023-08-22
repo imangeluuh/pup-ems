1d772f886efce2c37e6a5b751c401b2252427491
Angeluuh <angelanicolebatoon@gmail.com>
2023-08-22 23:27:26 +0800

    style: change style of admin login page

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, Email, DataRequired
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email')
-    password = PasswordField(label='Password')
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField(label='Sign in')
