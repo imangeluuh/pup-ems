0ed7ae771ed42a56ffa4db78d353de38717b7ed0
Angeluuh <angelanicolebatoon@gmail.com>
2023-08-28 23:43:35 +0800

    refactor: rename project lib to program v2

--- app/projects/forms.py
+++
@@ -1,3 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, DateField, SelectField, SubmitField
-from wtforms.validators import Length, EqualTo, DataRequired, ValidationError, Regexp
