14bf80e0008d972790269cb5dca89cd07d6e7c49
Angeluuh <angelanicolebatoon@gmail.com>
2023-08-31 02:25:40 +0800

    feat: add admin program management crud operation

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, url_for, request, redirect, flash
 from .forms import LoginForm, addProjectForm
-from ..models import Login, ExtensionProgram, Beneficiary
+from ..models import Login, ExtensionProgram, Beneficiary, Activity
 from app import db
-from flask_login import current_user, login_user, login_required
+from datetime import date
+from flask_login import current_user, login_user, login_required, logout_user
 
 admin_bp = Blueprint('admin', __name__, template_folder="templates", static_folder="static", static_url_path='static')
 
@@ -16,11 +17,11 @@ def adminLogin():
 
     if request.method == "POST":
         if form.validate_on_submit():
-            attempted_user = Login.query.filter_by(email=form.email.data).first()
-            if attempted_user and attempted_user.role_id == 1: 
+            attempted_user = Login.query.filter_by(Email=form.email.data).first()
+            if attempted_user and attempted_user.RoleId == 1: 
                 if attempted_user.check_password_correction(attempted_password=form.password.data):
                     login_user(attempted_user)
-                    return redirect(url_for('admin.projects')) # temp route
+                    return redirect(url_for('admin.programs')) # temp route
                 else:
                     flash('The password you\'ve entered is incorrect.')
             else:
@@ -29,7 +30,8 @@ def adminLogin():
 
 @admin_bp.route('/logout')
 def logout():
-    return "Use this to log out"
+    logout_user()
+    return redirect(url_for('admin.adminLogin'))
 
 @login_required
 @admin_bp.route('/home')
@@ -37,33 +39,92 @@ def home():
     pass
 
 @login_required
-@admin_bp.route('/projects', methods=['GET', 'POST'])
-def projects():
-    pass
-    # form = addProjectForm()
-    # # programs = ExtensionProgram.query.all()
-    # # if request.method == "POST":
-    # #     if form.validate_on_submit():
-    # #         program_to_add = ExtensionProgram(name = form.program_name.data,
-    # #                             status = form.status.data, 
-    # #                             target_participant = form.target_participant.data,
-    # #                             location = form.location.data,
-    # #                             start_date = form.start_date.data,
-    # #                             end_date = form.end_date.data,
-    # #                             description = form.description.data,
-    # #                             objectives = form.objectives.data,
-    # #                             expected_outcome = form.expected_outcome.data,
-    # #                             terms_conditions = form.terms_conditions.data)
-    # #         db.session.add(program_to_add)
-    # #         db.session.commit()
-    # #         return redirect(url_for('admin.projects'))
-    #     if form.errors != {}: # If there are errors from the validations
-    #         for err_msg in form.errors.values():
-    #             flash(err_msg)
-    # return render_template('admin/program_management.html', programs=programs, form=form)
+@admin_bp.route('/programs')
+def programs():
+    # pass
+    form = addProjectForm()
+    programs = ExtensionProgram.query.all()
+    
+    return render_template('admin/program_management.html', programs=programs, form=form)
+
+
+@login_required
+@admin_bp.route('/extension-program/insert', methods=['POST'])
+def insertExtensionProgram():
+    form = addProjectForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            program_to_add = ExtensionProgram(Name = form.program_name.data,
+                                Status = form.status.data, 
+                                Agenda = form.agenda.data,
+                                Program = form.program.data,
+                                DateApproved = form.date_approved.data,
+                                ImplementationDate = form.implementation_date.data,)
+            db.session.add(program_to_add)
+            db.session.commit()
+            return redirect(url_for('admin.programs'))
+        if form.errors != {}: # If there are errors from the validations
+            for err_msg in form.errors.values():
+                flash(err_msg)
+    return redirect('admin.programs')
+
+
+@login_required
+@admin_bp.route('/update/extension-program/<int:id>', methods=['POST'])
+def updateExtensionProgram(id):
+    form = addProjectForm()
+    extension_program = ExtensionProgram.query.get_or_404(id)
+    if form.validate_on_submit():
+        extension_program.Name = form.program_name.data
+        extension_program.Status = form.status.data
+        extension_program.Agenda = form.agenda.data
+        extension_program.Program = form.program.data
+        extension_program.DateApproved = form.date_approved.data
+        extension_program.ImplementationDate = form.implementation_date.data
+
+        try:
+            db.session.commit()
+            flash('Extension progam is successfully updated.')
+        except:
+            flash('There was an issue updating the extension program.')
+
+        return redirect(url_for('admin.programs'))
+    
+    if form.errors != {}: # If there are errors from the validations
+        for err_msg in form.errors.values():
+            flash(err_msg)
+
+    return redirect(url_for('admin.programs'))
+
+
+@login_required
+@admin_bp.route('/delete/extension-program/<int:id>', methods=['POST'])
+def deleteExtensionProgram(id):
+    extension_program = ExtensionProgram.query.get_or_404(id)
+    try:
+        db.session.delete(extension_program)
+        db.session.commit()
+        flash('Extension program is successfully deleted.')
+    except:
+        flash('There was an issue deleting the extension program.')
+
+    return redirect(url_for('admin.programs'))
+
 
 @login_required
 @admin_bp.route('/beneficiaries')
-def benificiaries():
-    benificiaries = Beneficiary.query.all()
-    return render_template('admin/benificiaries.html', benificiaries=benificiaries)
+def beneficiaries():
+    beneficiaries = Beneficiary.query.all()
+    return render_template('admin/beneficiaries.html', beneficiaries=beneficiaries)
+
+
+@admin_bp.route('/calendar')
+def calendar():
+    return render_template('admin/index.html')
+
+def fetch_activities():
+    return Activity.query.with_entities(Activity.activity_name, Activity.activity_date, Activity.location, Activity.activity_description).all()
+
+def fetch_today_activities():
+    today = date.today()
+    return Activity.query.filter_by(activity_date=today).with_entities(Activity.activity_name, Activity.location, Activity.activity_description).all()
