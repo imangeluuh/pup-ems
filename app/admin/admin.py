88ffeafba59361a426f793ceff46b894babc7ee4
Angeluuh <angelanicolebatoon@gmail.com>
2023-09-01 16:06:19 +0800

    refactor: change program management ui

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, url_for, request, redirect, flash
-from .forms import LoginForm, ProjectForm
-from ..models import Login, ExtensionProgram, Beneficiary
+from .forms import LoginForm, ProgramForm, ProjectForm
+from ..models import Login, ExtensionProgram, Beneficiary, Project, Agenda, Program
 from app import db
 from datetime import date
 from flask_login import current_user, login_user, login_required, logout_user
@@ -41,23 +41,24 @@ def home():
 @login_required
 @admin_bp.route('/programs')
 def programs():
-    # pass
-    form = ProjectForm()
+    form = ProgramForm()
+    project_form = ProjectForm()
     programs = ExtensionProgram.query.all()
+    projects = Project.query.all()
     
-    return render_template('admin/program_management.html', programs=programs, form=form)
+    return render_template('admin/program_management.html', programs=programs, projects=projects, form=form, project_form=project_form)
 
 
 @login_required
 @admin_bp.route('/extension-program/insert', methods=['POST'])
 def insertExtensionProgram():
-    form = ProjectForm()
+    form = ProgramForm()
     if request.method == "POST":
         if form.validate_on_submit():
             program_to_add = ExtensionProgram(Name = form.program_name.data,
                                 Status = form.status.data, 
-                                Agenda = form.agenda.data,
-                                Program = form.program.data,
+                                AgendaId = form.agenda.data,
+                                ProgramId = form.program.data,
                                 DateApproved = form.date_approved.data,
                                 ImplementationDate = form.implementation_date.data,)
             db.session.add(program_to_add)
@@ -73,13 +74,13 @@ def insertExtensionProgram():
 @login_required
 @admin_bp.route('/update/extension-program/<int:id>', methods=['POST'])
 def updateExtensionProgram(id):
-    form = ProjectForm()
+    form = ProgramForm()
     extension_program = ExtensionProgram.query.get_or_404(id)
     if form.validate_on_submit():
         extension_program.Name = form.program_name.data
         extension_program.Status = form.status.data
-        extension_program.Agenda = form.agenda.data
-        extension_program.Program = form.program.data
+        extension_program.AgendaId = int(form.agenda.data)
+        extension_program.ProgramId = int(form.program.data)
         extension_program.DateApproved = form.date_approved.data
         extension_program.ImplementationDate = form.implementation_date.data
 
@@ -108,6 +109,78 @@ def deleteExtensionProgram(id):
         flash('Extension program is successfully deleted.', category='success')
     except:
         flash('There was an issue deleting the extension program.', category='error')
+
+    return redirect(url_for('admin.programs'))
+
+
+@login_required
+@admin_bp.route('/extension-program/project/insert', methods=['POST'])
+def insertProject():
+    form = ProjectForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            project_to_add = Project(Name = form.project_name.data,
+                                LeadProponent = form.lead_proponent.data, 
+                                ProjectType = form.project_type.data,
+                                Rationale = form.rationale.data,
+                                Objectives = form.objectives.data,
+                                StartDate = form.start_date.data,
+                                NumberOfBeneficiaries = form.num_of_beneficiaries.data,
+                                BeneficiariesClassifications = form.beneficiaries_classifications.data,
+                                ProjectScope = form.project_scope.data,
+                                ExtensionProgramId = form.extension_program.data)
+            db.session.add(project_to_add)
+            db.session.commit()
+            flash('Extension project is successfully inserted.', category='success')
+            return redirect(url_for('admin.programs'))
+        if form.errors != {}: # If there are errors from the validations
+            for err_msg in form.errors.values():
+                flash(err_msg, category='error')
+    return redirect(url_for('admin.programs'))
+
+
+@login_required
+@admin_bp.route('/update/extension-program/project/<int:id>', methods=['POST'])
+def updateProject(id):
+    form = ProjectForm()
+    extension_project = Project.query.get_or_404(id)
+    if form.validate_on_submit():
+        extension_project.Name = form.project_name.data
+        extension_project.LeadProponent= form.lead_proponent.data
+        extension_project.ProjectType = form.project_type.data
+        extension_project.Rationale = form.rationale.data
+        extension_project.Objectives = form.objectives.data
+        extension_project.StartDate = form.start_date.data
+        extension_project.NumberOfBeneficiaries = form.num_of_beneficiaries.data
+        extension_project.BeneficiariesClassifications = form.beneficiaries_classifications.data
+        extension_project.ProjectScope = form.project_scope.data
+        extension_project.ExtensionProgramId = form.extension_program.data
+
+        try:
+            db.session.commit()
+            flash('Extension progaject is successfully updated.', category='success')
+        except:
+            flash('There was an issue updating the extension project.', category='error')
+
+        return redirect(url_for('admin.programs'))
+    
+    if form.errors != {}: # If there are errors from the validations
+        for err_msg in form.errors.values():
+            flash(err_msg)
+
+    return redirect(url_for('admin.programs'))
+
+
+@login_required
+@admin_bp.route('/delete/extension-program/project/<int:id>', methods=['POST'])
+def deleteProject(id):
+    project = Project.query.get_or_404(id)
+    try:
+        db.session.delete(project)
+        db.session.commit()
+        flash('Extension project is successfully deleted.', category='success')
+    except:
+        flash('There was an issue deleting the extension project.', category='error')
 
     return redirect(url_for('admin.programs'))
 
--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,6 +1,8 @@
+from flask import current_app
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, DateField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, DateField, SelectField, TextAreaField, IntegerField, HiddenField
 from wtforms.validators import Length, Email, DataRequired, Optional
+from ..models import Agenda, Program
 
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired(), Email()])
@@ -8,11 +10,35 @@ class LoginForm(FlaskForm):
     submit = SubmitField(label='Sign in')
 
 
-class ProjectForm(FlaskForm):
+class ProgramForm(FlaskForm):
     program_name = StringField(label='Extension Program Name', validators=[DataRequired()])
-    agenda = StringField(label='Agenda', validators=[DataRequired()])
-    program = StringField(label='Program', validators=[DataRequired()])
+    agenda = SelectField(label='Agenda', validators=[DataRequired()])
+    program = SelectField(label='Program', validators=[DataRequired()])
     date_approved = DateField(label='Date Approved', validators=[Optional()])
     implementation_date = DateField(label='Implementation Date', validators=[Optional()])
-    status = StringField(label="Status", validators=[DataRequired()])
+    status = StringField(label='Status', validators=[DataRequired()])
     submit = SubmitField(label='Save Program')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        with current_app.app_context():
+            self.agenda.choices = [(agenda.AgendaId, agenda.AgendaName) for agenda in Agenda.query.all()]
+            self.program.choices = [(program.ProgramId, program.ProgramName) for program in Program.query.all()]
+
+
+class ProjectForm(FlaskForm):
+    project_name = StringField(label="Project Name", validators=[DataRequired()])
+    lead_proponent = StringField(label="Lead Proponent", validators=[DataRequired()])
+    project_type = SelectField(label="Project Type", choices=[('Need-Based', 'Need-Based'),
+                                                            ('Quick Response', 'Quick Response'),
+                                                            ('Other Related Activities/Collaboration', 'Other Related Activities/Collaboration')],
+                                                            validators=[DataRequired()])
+    rationale = TextAreaField(label="Rationale / Description of the Project", validators=[DataRequired()])
+    objectives = TextAreaField(label="Objective of the Project", validators=[DataRequired()])
+    start_date = DateField(label="Start Date", validators=[Optional()])
+    num_of_beneficiaries = IntegerField(label="Specific Numbers of Target Beneficiaries", validators=[Optional()])
+    beneficiaries_classifications = StringField(label="Target Beneficiaries Classifications", validators=[Optional()])
+    project_scope = TextAreaField(label="Scope of Project", validators=[DataRequired()])
+    extension_program = HiddenField(validators=[DataRequired()])
+    submit = SubmitField(label="Save Project") 
