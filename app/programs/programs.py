9c47f6e8d75712c7b38ba4e2b9790069fea0fc38
Angeluuh <angelanicolebatoon@gmail.com>
2023-09-07 19:05:08 +0800

    feat: beneficiary project registration

--- app/programs/programs.py
+++ app/programs/programs.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template, url_for, request, redirect, flash
 from flask_login import login_user, logout_user, current_user
-from ..models import Project, ExtensionProgram, Program
+from ..models import Project, ExtensionProgram, Program, Registration
 import calendar
 from datetime import datetime
+from sqlalchemy import extract
+from app import db
 
 programs_bp = Blueprint('programs', __name__, template_folder="templates", static_folder="static", static_url_path='static')
 
@@ -15,19 +17,26 @@ def programsList():
 def projectsList():
     projects = Project.query.all()
 
-    if request.method == 'POST':
-        dict_filter_conditions = {}
+    if 'submit' in request.args:
+        dict_filter_conditions = {} 
 
-        filter_value = request.form.get('filter')  
-        option_value = request.form.get('option') 
-
-        if filter_value == 'All':
+        filter_value = request.args.get('filter')  
+        option_value = request.args.get('option') 
+        if filter_value == "All":
             return render_template('programs/projects_list.html', projects=projects)
-        elif not filter_value == "":
-            dict_filter_conditions[filter_value] = option_value
+        elif filter_value == "Month":
+            dict_month = dict((month, index) for index, month in enumerate(calendar.month_name) if month)
+            # Convert the selected option to a month number (1 for January, 2 for February, etc.)
+            int_month = dict_month[option_value]
+            dict_filter_conditions['Month'] =  extract('month', Project.StartDate) == int_month
+        elif filter_value == "ExtensionProgram":
+            dict_filter_conditions['ExtensionProgram'] =  Project.ExtensionProgram.Name = option_value
+
 
         # Get the projects based on the selected filter and option
         filtered_projects = Project.query.filter(dict_filter_conditions.get(filter_value)).all()
+
+        return render_template('programs/projects_list.html', projects=filtered_projects)
 
     return render_template('programs/projects_list.html', projects=projects)
 
@@ -59,4 +68,24 @@ def filters():
 
     return render_template('programs/filters.html',list_filter=list_filter)
 
+
+@programs_bp.route('/registration/<int:project_id>', methods=['GET', 'POST'])
+def registration(project_id):
+    project = Project.query.get_or_404(project_id)
+    user_id = current_user.UserLogin[0].UserId if current_user.is_authenticated else None
+    bool_is_registered = True if Registration.query.filter_by(ProjectId=project_id, UserId=user_id).first() else False
+    print( project_id, user_id)
+    if request.method == 'POST':
+        registration_to_create = Registration(ProjectId = project_id,
+                                            UserId=user_id)
+        try:
+            db.session.add(registration_to_create)
+            db.session.commit()
+            flash('Registration Successful!', category='success')
+        except  Exception as e:
+            flash('There was an issue during registration.', category='error')
+            print('There was an issue during registration.', str(e))
+
+        return redirect(url_for('programs.registration', project_id=project_id))
+    return render_template("programs/project_reg.html", project=project, bool_is_registered=bool_is_registered)
 
