c155ce3e27e77de3d6ab7ae89dda48c9e4fe7f5e
Angeluuh <angelanicolebatoon@gmail.com>
2023-09-24 13:52:54 +0800

    refactor: send email asynchronously

--- app/email.py
+++ app/email.py
@@ -2,14 +2,15 @@ from flask_mail import Message
 from app import mail, app
 from threading import Thread
 
+# Function to send an email asynchronously
 def sendAsyncEmail(app, msg):
     with app.app_context():
         mail.send(msg)
 
-
+# Function to send an email asynchronously with threading
 def sendEmail(subject, recipients, text_body, html_body):
     msg=Message(subject, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
-    # Thread(target=sendAsyncEmail, args=(app, msg)).start()
+    # Create a new Thread to send the email asynchronously
+    Thread(target=sendAsyncEmail, args=(app, msg)).start()
