caaff5cc81cb1032ad26abc18396a554fe52b573
Angeluuh <angelanicolebatoon@gmail.com>
2023-10-02 23:45:11 +0800

    feat: email for reset password

--- app/email.py
+++ app/email.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask_mail import Message
 from app import mail, app
 from threading import Thread
@@ -14,3 +15,10 @@ def sendEmail(subject, recipients, text_body, html_body):
     msg.html = html_body
     # Create a new Thread to send the email asynchronously
     Thread(target=sendAsyncEmail, args=(app, msg)).start()
+
+def sendPasswordResetEmail(user):
+    token = user.get_reset_password_token()
+    sendEmail('[PUPQC-ESIS] Reset Your Password',
+                recipients=[user.Email],
+                text_body=render_template('email/reset_password.txt', user=user, token=token),
+                html_body=render_template('email/reset_password.html', user=user, token=token))
