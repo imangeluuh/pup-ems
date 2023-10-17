5371242dbc193ce4d71d329006b2c83a10c9a545
Angeluuh <101722896+imangeluuh@users.noreply.github.com>
2023-10-17 15:46:25 +0800

    feat: extension programs budget allocation

--- app/programs/programs.py
+++ app/programs/programs.py
@@ -445,6 +445,18 @@ def deleteActivity(id):
 
     return redirect(url_for('programs.viewProject', id=project_id))
 
+@bp.route('/budget-allocation')
+@login_required(role=["Admin"])
+def budgetAllocation():
+    ext_programs = ExtensionProgram.query.all()
+    float_total_proposed_budget = 0
+    float_total_approved_budget = 0
+    for ext_program in ext_programs:
+        float_total_proposed_budget += ext_program.ProposedBudget
+        float_total_approved_budget += ext_program.ApprovedBudget
+    return render_template('admin/budget_allocation.html', ext_programs=ext_programs, float_total_approved_budget=float_total_approved_budget, float_total_proposed_budget=float_total_proposed_budget)
+
+
 
 
 # ======================= User views ======================
